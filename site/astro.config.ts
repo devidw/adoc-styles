638388664e3fea5d12108ca148c5b02918cce10b
David Wolf <68775926+devidw@users.noreply.github.com>
2022-10-31 13:57:35 +0000

    update 6 files and delete 1 file

--- site/astro.config.ts
+++ site/astro.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
-import image from "@astrojs/image"
+// import image from "@astrojs/image"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), svelte(), tailwind()],
-  // site: 'https://devidw.github.io',
-  // base: '/adoc-styles'
+  integrations: [svelte(), tailwind()],
 })
